66a8c7a281f8ad25e729be5bebd8f0624e859095
mdmehedi31 <mdmehedihasan.mh31@gmail.com>
2026-06-07 10:07:11 +0600

    generate key by using SpEL key

--- src/main/java/com/spc/service/UserService.java
+++ src/main/java/com/spc/service/UserService.java
@@ -3,8 +3,10 @@ package com.spc.service;
 import com.spc.entity.UserEntity;
 import com.spc.entity.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.SimpleKeyGenerator;
 import org.springframework.stereotype.Service;
 
 
@@ -15,8 +17,8 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-
-    @Cacheable(cacheNames = "user")
+    // Key is SpEL
+    @Cacheable(cacheNames = "user", key = "#userId")
     public UserEntity getUserEntityById(Integer userId){
         return this.userRepository.findById(userId).orElse(null);
     }
